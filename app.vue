29f5eb59109df154e71f9f8672990f1fd90ce697
YinCheng <eason010630@gmail.com>
2024-12-18 14:40:17 +0800

    Updata: Nuxt

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
+<script setup lang="ts">
+  useSeoMeta({
+    title: 'YinCheng',
+    ogTitle: 'YinCheng',
+    description: '',
+    ogDescription: '',
+    ogImage: ''
+  });
+</script>
+
 <template>
   <div>
+    <div class="flex p-2 m-2 bg-neutral-400 rounded-xl">
+      <img class="w-12 h-12 rounded-full" src="https://avatars.githubusercontent.com/u/99303523">
+      <h1 class="text-xl font-bold justify-center p-2">YinCheng</h1>
+    </div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
