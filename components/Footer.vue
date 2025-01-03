b8ade2028576f2fa541429573dacaab3263ea1da
YinCheng <eason010630@gmail.com>
2025-01-04 01:11:25 +0800

    add: 404 page

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,7 +6,6 @@
     <div class="flex justify-between items-center p-4 border-t-2 border-neutral-400">
         <p>© 2025 YinCheng.</p>
         <div class="flex flex-wrap gap-2">
-            <NuxtLink class="link" to="/">首頁</NuxtLink>
             <NuxtLink class="link" to="/about">關於</NuxtLink>
             <NuxtLink class="link" to="/terms">條款</NuxtLink>
         </div>
