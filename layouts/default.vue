86170d1c79e4beafb22a709fba6323ce7dec90ec
YinCheng <eason010630@gmail.com>
2025-01-04 00:27:36 +0800

    add: snowflask

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,53 @@
   import { SpeedInsights } from "@vercel/speed-insights/nuxt";
   import Header from "../components/Header.vue";
   import Footer from "../components/Footer.vue";
+
+  const month = new Date().getMonth();
+  let snows = false;
+  if(month === 10 || month === 11 || month === 0) {
+    snows = true; // 11, 12, 1
+  }
 </script>
 
 <template>
+  <div class="snowflakes" aria-hidden="true" :hidden="!snows">
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">冷</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">冷</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+    <div class="snowflake">
+      <div class="inner">❅</div>
+    </div>
+  </div>
   <div class="min-h-screen flex flex-col p-4">
     <Header />
     <main class="flex-grow p-4">
