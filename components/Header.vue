81b4a6405718915f6401579376bc887294fa08c0
YinCheng <eason010630@gmail.com>
2025-01-05 00:38:41 +0800

    add: BlogLink

--- components/Header.vue
+++ components/Header.vue
@@ -2,10 +2,15 @@
 
 <template>
     <div>
-        <NuxtLink class="link" to="/">
-            <div class="flex p-2 m-2">
-                <img class="w-12 h-12 rounded-full" src="https://avatars.githubusercontent.com/u/99303523" />
-                <h1 class="text-xl font-bold justify-center p-2">YinCheng</h1>
+        <NuxtLink class="link" href="/">
+            <div class="flex justify-between items-center">
+                <div class="flex p-2 m-2">
+                    <img class="w-12 h-12 rounded-full" src="https://avatars.githubusercontent.com/u/99303523" />
+                    <h1 class="text-xl font-bold justify-center p-2">YinCheng</h1>
+                </div>
+                <div class="justify-center p-2 gap-2">
+                    <NuxtLink class="link" to="/blog">部落格</NuxtLink>
+                </div>
             </div>
         </NuxtLink>
     </div>
