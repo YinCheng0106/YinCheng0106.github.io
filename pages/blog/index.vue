b437309881b5f832461941a27feb5a8f382c4e8d
YinCheng <eason010630@gmail.com>
2025-01-09 20:01:00 +0800

    fix: blog css

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,7 +5,7 @@
     <div>
         <ContentList path="/blog" v-slot="{ list }">
             <div v-for="blog in list" :key="blog._path">
-                <NuxtLink :to="blog._path" class="block p-4 border-b border-gray-200 dark:border-gray-800">
+                <NuxtLink :to="blog._path" class="block p-2 m-4 border-b border-gray-200 rounded-t-2xl dark:hover:bg-gray-950 hover:bg-gray-100 dark:hover:border-gray-500 hover:border-gray-400 duration-300 delay-75">
                     <h2 class="m-2 font-bold text-xl">{{ blog.title }}</h2>
                     <p class="px-4 py-2 text-xs dark:text-gray-400 text-gray-700">{{ blog.description }}</p>
                 </NuxtLink>
