7a33d87692e93ea4f4a51b78127cc4c2de7e728b
YinCheng0106 <eason010630@gmail.com>
2025-01-15 03:11:41 +0800

    fix: bg-theme

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -8,48 +8,11 @@
   if(month === 10 || month === 11 || month === 0) {
     snows = true; // 11, 12, 1
   }
+  const isDark = computed(() => useColorMode().value == "dark");
 </script>
 
 <template>
   <div>
-    <div class="snowflakes" aria-hidden="true" :hidden="!snows">
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">冷</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">冷</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-      <div class="snowflake">
-        <div class="inner">❅</div>
-      </div>
-    </div>
     <div class="min-h-screen flex flex-col p-4">
       <Header />
       <main class="flex-grow p-4">
@@ -58,5 +21,22 @@
       <Footer />
       <SpeedInsights />
     </div>
+    <SnowfallBg
+      :color="isDark ? '#FFFFFF' : '#000000'"
+      class="fixed inset-0 pointer-events-none"
+      :hidden="!snows"
+      :min-radius="0.2"
+      :max-radius="3"
+      :speed="0.75"
+    />
+    <ParticlesBg
+      class="fixed inset-0 pointer-events-none"
+      :hidden="snows"
+      :quantity="100"
+      :ease="100"
+      :color="isDark ? '#FFF' : '#000'"
+      :staticity="10"
+      refresh
+    />
   </div>
 </template>
