60ad23e68a17fc3cfef47b0fe9427e4498907dbb
YinCheng0106 <eason010630@gmail.com>
2025-01-16 11:40:13 +0800

    fix: WTF

--- src/app.vue
+++ src/app.vue
@@ -4,7 +4,7 @@ useSeoMeta({
   ogTitle: "YinCheng",
   description: "這是一個關於YinCheng的網站",
   ogDescription: "這是一個關於YinCheng的網站",
-  ogImage: '/index.png',
+  ogImage: '/og-image.png',
   twitterCard: 'summary_large_image',
 });
 </script>
