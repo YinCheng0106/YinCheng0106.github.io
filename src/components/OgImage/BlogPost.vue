e1f2609f409e2ae83166d6771fb3011e0d2c6de4
YinCheng <eason010630@gmail.com>
2025-01-18 14:59:24 +0800

    fix: OgImage

--- src/components/OgImage/BlogPost.vue
+++ src/components/OgImage/BlogPost.vue
@@ -15,21 +15,21 @@ withDefaults(defineProps<{
   <div class="h-full w-full flex items-start justify-start border-solid border-[12px] bg-zinc-900 text-gray-50">
     <div class="flex items-start justify-start w-full h-full">
       <div class="flex flex-col w-full h-full">
-        <div>
-          <h1 class="text-[80px] font-black px-20 pt-20 text-left">
+        <div class="pt-20">
+          <img
+              v-if="logo"
+              :src="logo"
+              class="absolute top-0 right-0 p-10"
+              style="height: 256px; width: 256px; border-radius: 4rem;"
+          >
+          <h1 class="text-[70px] font-black px-20 pt-20 text-left">
             {{ title }}
           </h1>
-          <img
-            v-if="logo"
-            :src="logo"
-            class="absolute bottom-0 right-0 p-10"
-            style="height: 256px; width: 256px; border-radius: 4rem;"
-          >
+          <p class="text-4xl px-20 text-left text-gray-400">
+            {{ description }}
+          </p>
         </div>
-        <p class="text-4xl px-20 text-left text-gray-400">
-          {{ description }}
-        </p>
-        <p class="justify-end text-2xl pb-10 pt-40 px-10 font-bold mb-0">
+        <p class="justify-end text-2xl pt-20 px-10 font-bold mb-0">
           yincheng.vercel.app
         </p>
       </div>
