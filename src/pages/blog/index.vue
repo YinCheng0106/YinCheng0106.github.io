b38c54a2be2cecd72d3204c93a8a34e0e94cefb6
YinCheng0106 <eason010630@gmail.com>
2025-01-20 00:55:47 +0800

    add: BlogListDate

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Clock } from 'lucide-vue-next';
+
 useSeoMeta({
   title: "部落格",
   ogTitle: "部落格 | YinCheng",
@@ -16,10 +18,15 @@ useSeoMeta({
         <NuxtLink
           :to="blog._path"
           class="block p-2 m-4 border-b border-gray-200 rounded-t-2xl dark:hover:bg-neutral-900 hover:bg-gray-100 dark:hover:border-gray-500 hover:border-gray-400 duration-300 transition ease-in-out"
-        >
-          <h2 class="m-2 font-bold text-xl">{{ blog.title }}</h2>
-          <p class="px-4 py-2 text-xs dark:text-gray-400 text-gray-700">
-            {{ blog.description }}
+        > 
+          <h2 class="mt-2 mx-2 font-bold text-xl">{{ blog.title }}</h2>
+          <div class="px-4 py-2">
+            <p class="text-xs line-clamp-2 dark:text-gray-400 text-gray-700">
+              {{ blog.description }}
+            </p>
+          </div>
+          <p class="flex flex-wrap items-center px-4 text-xs dark:text-gray-400 text-gray-700">
+            <Clock class="mr-1" :size="16" :stroke-width="2.25" />{{ blog.date }}
           </p>
         </NuxtLink>
       </div>
