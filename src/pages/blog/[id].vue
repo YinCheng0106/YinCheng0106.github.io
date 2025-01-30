4a1d410da6c8199ad6e1b92165159d1f2297459b
YinCheng <eason010630@gmail.com>
2025-01-30 23:54:00 +0800

    Fix BlogPosts

--- src/pages/blog/[id].vue
+++ src/pages/blog/[id].vue
@@ -16,6 +16,8 @@ const { data: posts } = await useAsyncData(route.path, () =>
   .path(route.path)
   .first());
 
+console.log(posts);
+
 if(posts.value?.title === undefined) {
   useSeoMeta({
     title: "404 Not Found",
