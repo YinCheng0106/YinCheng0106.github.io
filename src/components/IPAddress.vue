709ab45e58d3ea7de979cb50b8ad4005a6747f27
YinCheng <eason010630@gmail.com>
2025-02-19 14:52:24 +0800

    Fix IPAddress Component

--- src/components/IPAddress.vue
+++ src/components/IPAddress.vue
@@ -1,22 +1,29 @@
 <script lang="ts" setup>
 const ip = ref('Loading...');
+const country = ref('Loading...');
 
 onMounted(async () => {
   try {
     const data = await $fetch('/api/get-ip');
-    if(data.ip !== null) {
+    if (data.ip) {
       ip.value = data.ip;
       console.log("S: ", data.ip);
     } else {
       ip.value = '無法取得 IP 地址';
       console.log("E: ", data);
+      return;
     }
+    const geoData = await $fetch(`http://www.geoplugin.net/json.gp?ip=61.221.28.190`);
+    console.log(geoData);
+    country.value = geoData.geoplugin_countryName || '未知國家';
   } catch (error) {
-    ip.value = '錯誤 無法取得 IP 地址';
+    console.error('Error fetching IP or country:', error);
+    country.value = 'Not Found';
   }
 });
 </script>
 
 <template>
   <div class="font-mono">{{ ip }}</div>
+  <div class="font-mono">{{ country }}</div>
 </template>
--- src/pages/tools/ip.vue
+++ src/pages/tools/ip.vue
@@ -1,10 +1,15 @@
 <script lang="ts" setup>
-
+useSeoMeta({
+  title: "IP 位置",
+  ogTitle: "IP 位置 | YinCheng",
+  description: "查看你的 IP 位址",
+  ogDescription: "查看你的 IP 位址",
+})
 </script>
 
 <template>
   <div class="w-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-    <h2 class="text-4xl md:text-5xl lg:text-6xl font-bold my-2">IP Address</h2>
+    <h2 class="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">IP Address</h2>
     <div class="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 my-2">
       <p class="text-md md:text-lg lg:text-xl"><IPAddress /></p>
     </div>
