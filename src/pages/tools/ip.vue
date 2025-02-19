87855d66e6355da9a4b46e469f71326e36f3d8fc
YinCheng <eason010630@gmail.com>
2025-02-19 09:58:00 +0800

    Fix IP Page Style

--- src/pages/tools/ip.vue
+++ src/pages/tools/ip.vue
@@ -4,7 +4,9 @@
 
 <template>
   <div class="w-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-    <h2 class="text-4xl">IP Address</h2>
-    <p ><IPAddress /></p>
+    <h2 class="text-4xl md:text-5xl lg:text-6xl font-bold my-2">IP Address</h2>
+    <div class="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 my-2">
+      <p class="text-md md:text-lg lg:text-xl"><IPAddress /></p>
+    </div>
   </div>
 </template>
