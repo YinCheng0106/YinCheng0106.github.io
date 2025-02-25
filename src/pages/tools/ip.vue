0172a8dbc3c980713e95c41da8a73dae34ccfb4e
YinCheng <eason010630@gmail.com>
2025-02-25 20:39:36 +0800

    Fix IP component

--- src/pages/tools/ip.vue
+++ src/pages/tools/ip.vue
@@ -9,7 +9,7 @@ useSeoMeta({
 
 <template>
   <div class="w-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-    <h2 class="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">IP Address</h2>
+    <h2 class="flex flex-wrap justify-center items-center text-4xl md:text-5xl lg:text-6xl font-bold mb-4">IP Address</h2>
     <IPAddress />
     <p class="flex flex-wrap justify-center items-center mt-4 text-xs dark:text-gray-500 text-gray-600">此為測試功能，可能會發生錯誤</p>
   </div>
